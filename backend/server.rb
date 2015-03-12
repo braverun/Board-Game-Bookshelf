3aa32101b31323a15fcfb9ce7f363063f90f1624
Weston Humphreys <braverun@gmail.com>
2015-03-12 18:12:31 -0400

    Create ability to search for games by name and return all of the info in the game item

--- backend/server.rb
+++ backend/server.rb
@@ -5,5 +5,8 @@ require "bgg-api"
 get "/search/:title" do |title|
   content_type :json
   bgg = BggApi.new
-  bgg.search( {query: title, type: 'boardgame'} ).to_json
+  items = bgg.search( {query: title, type: 'boardgame'} )["item"] || []
+  return items.take(10).map do |game|
+    bgg.thing({id: game["id"]})["item"].first
+  end.to_json
 end
