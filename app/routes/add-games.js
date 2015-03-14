1451d3bdbf340d15dc0462033ec9735d26712f3d
Weston Humphreys <braverun@gmail.com>
2015-03-14 13:58:35 -0400

    Started work on connecting to apis

--- app/routes/add-games.js
+++ app/routes/add-games.js
@@ -1,4 +1,11 @@
+import ajax from 'ic-ajax';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  findAll: function(name) {
+      /* jshint unused: false */
+      return ajax("https://board-game-bookshelf.herokuapp.com/search");
+  }
+  
 });
