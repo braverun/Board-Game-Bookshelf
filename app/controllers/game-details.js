0bd3b4882184978526c8a3f19cd166cca57c4954
Weston Humphreys <braverun@gmail.com>
2015-03-23 10:52:29 -0400

    Cool stuff

--- app/controllers/game-details.js
+++ app/controllers/game-details.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import ajax from 'ic-ajax';
 
 export default Ember.Controller.extend({
+  isEditing: false,
+
   actions: {
     destroy: function() {
       /* jshint unused: false */
@@ -10,7 +12,26 @@ export default Ember.Controller.extend({
       return ajax({
         url: "https://api.parse.com/1/classes/Game/" + model.id,
         type: "DELETE"
-      }).then(self.transitionToRoute('index')).then(alert("Your game has been destroyed!"));
+      }).then(alert('Game has been removed')).done(self.transitionToRoute('index'));
     },
-    }
+
+    edit: function() {
+      /* jshint unused: false */
+      // var notes = this.get('js-user-notes');
+      this.set('isEditing', true);
+      console.log(notes);
+    },
+
+    save: function() {
+      /* jshint unused: false */
+      var model = this.get('model');
+      // TODO: this.('model').save();
+      return ajax ({
+        url: "https://api.parse.com/1/classes/Game/" + model.id,
+        type: "PUT",
+        data: JSON.stringify({userNotes: model.get('userNotes')})
+      });
+
+    },
+  }
 });
--- app/routes/game-details.js
+++ app/routes/game-details.js
@@ -3,13 +3,6 @@ import ajax from 'ic-ajax';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return ajax("https://api.parse.com/1/classes/Game/" + params.game_id).then(function(response){
-      // return response.results.map(function(game) {
-      //   game.id = game.objectId;
-      //   delete game.objectId;
-      //   return game;
-      // });
-      return response.results;
-    });
+    return ajax("https://api.parse.com/1/classes/Game/" + params.game_id);
   }
 });
